fa42274a00c71b4617406869621908348d811947
Stefanie Centi <stefcenti@centiumtech.com>
2016-07-29 00:19:23 -0400

    Modified findFriend() method of FriendFinder app to find a friend based on compatibility algorithm.

--- app/data/friends.js
+++ app/data/friends.js
@@ -52,7 +52,54 @@ function FriendFinder() {
 
 	this.findFriend = function(scores) {
 		console.log("findFriend()");
-		return this.friendList[(Math.floor(Math.random() * this.friendList.length) + 1)];
+
+		// This code returns a random friend:
+		// return this.friendList[(Math.floor(Math.random() * this.friendList.length) + 1)];
+
+		// This code follows the compatibility algorithm:
+		// Determine the user's most compatible friend using the following as a guide:
+
+		// - Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
+		// - With that done, compare the difference between current user's scores against those from 
+		//   other users, question by question. Add up the differences to calculate the totalDifference.
+		//
+		// Example:
+		// User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+		// User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+		// Total Difference: 2 + 1 + 2 = 5
+		//
+		// - Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
+		// - The closest match will be the user with the least amount of difference.
+		// - Once you've found the current user's most compatible friend, display the result as a modal 
+		//   pop-up.
+		//
+		// The modal should display both the name and picture of the closest match.
+
+		var fList = this.friendList;
+
+		var bestRating = 999; // Lowest compatibility rating so far.  Start higher than
+						  	  // any difference could ever be so when compared the first
+						  	  // time, the current friend being compared will be lower.
+		var bestMatch = -1; // Index of friend with lowest compatibility index;
+
+		for (var i=0; i<fList.length; i++) {
+			// Compare the scores of the current friend with the scores passed in from the survey.
+			var fScores = fList[i].scores;
+			var diff = 0;
+
+			for (var j=0; j<fScores.length; j++) {
+				diff += Math.abs(fScores[j] - scores[j]);
+			}
+
+			if (diff < bestRating) {
+				bestRating = diff; // if observed difference with the current friend is less than 
+								  // the minimum difference so far then set this as the new minimum 
+								  //  difference
+				bestIndex = i; // set friendList[i] as the user with smallest difference
+			}
+		}
+
+		return this.friendList[bestIndex];
 	}
 }
 
